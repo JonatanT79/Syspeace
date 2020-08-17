7a6e010e17b1932635371dae1e1c544f41253400
Jonathan Tran <jonathan.t79@hotmail.com>
2020-08-17 16:04:18 +0200

    Parsing LoginFile

---
+++ Syspeace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Syspeace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string LoginFile = @"C:\Users\jonat\source\repos\Syspeace\Syspeace\Data\LoginFile.txt";
+            
+            string[] TextList = File.ReadAllLines(LoginFile);
+            List<string> ObservationList = new List<string>();
+            int i = 1;
+            foreach (var item in TextList)
+            {
+                Console.WriteLine(item);
+                //if(i == 3)
+                //{
+                //    break;
+                //}
+                i++;
+            }
+
+        }
+    }
+}
+
+// Tid - ID - Status - Användarnamn - IP Adress
