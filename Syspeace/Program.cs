d4fa1af2eb1e34f47085f826139fcfd732c0e991
Jonathan Tran <jonathan.t79@hotmail.com>
2020-08-18 15:27:56 +0200

    Sorted all logs to see observation

--- Syspeace/Program.cs
+++ Syspeace/Program.cs
@@ -2,28 +2,53 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Syspeace
 {
     class Program
     {
+        const string LoginFile = @"C:\Users\jonat\source\repos\Syspeace\Syspeace\Data\LoginFile.txt";
+        static string[] TextList = File.ReadAllLines(LoginFile);
+
+        static List<string> SortedLogsByID = new List<string>();
+        static List<string> ObservationSort = new List<string>();
+        static List<string> FullObservation = new List<string>();
         static void Main(string[] args)
         {
-            const string LoginFile = @"C:\Users\jonat\source\repos\Syspeace\Syspeace\Data\LoginFile.txt";
-            
-            string[] TextList = File.ReadAllLines(LoginFile);
-            List<string> ObservationList = new List<string>();
-            int i = 1;
-            foreach (var item in TextList)
+            for (int i = 0; i < TextList.Length; i++)
             {
-                Console.WriteLine(item);
-                //if(i == 3)
-                //{
-                //    break;
-                //}
-                i++;
+                string LogRow = TextList[i];
+
+                if (LogRow.Contains("\tCONNECT"))
+                {
+                    ConnectSearch(LogRow);
+                }
+
+                //  Console.WriteLine(LogRow);
             }
 
+            ObservationSort = SortedLogsByID;
+            foreach (var item in ObservationSort)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void ConnectSearch(string Logrow)
+        {
+            Match match = Regex.Match(Logrow, @"\b\d...\b");
+            string ID = match.Value;
+            SearchLogByID(ID);
+        }
+        static void SearchLogByID(string ID)
+        {
+            foreach (var item in TextList)
+            {
+                if (item.Contains(ID))
+                {
+                    SortedLogsByID.Add(item);
+                }
+            }
         }
     }
 }
